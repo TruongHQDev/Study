cea348d0ba2f9b20cadb5b1d9092765aae5c41cc
AUTO SERVER <vt001@ASs-Mac-mini.local>
2019-11-25 15:02:25 +0700

    add folder and demo1 in imple controllers and views

---
+++ ImpleControllersAndViews/Demo1/Demo1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Demo1
+//
+//  Created by AUTO SERVER on 11/25/19.
+//  Copyright © 2019 AUTO SERVER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
