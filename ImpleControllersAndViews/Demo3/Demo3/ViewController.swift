b4d57411c3a4b73b1983cb14fb64db6a4222d3c3
AUTO SERVER <vt001@ASs-Mac-mini.local>
2019-11-26 16:27:05 +0700

    Done Demo 3

---
+++ ImpleControllersAndViews/Demo3/Demo3/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Demo3
+//
+//  Created by AUTO SERVER on 11/26/19.
+//  Copyright © 2019 AUTO SERVER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+       // Bundle.main.loadNibNamed("VC2ViewController", owner: self, options: nil)?.first
+    }
+
+    @IBAction func textDidEnd(_ sender: Any) {
+        
+    }
+    
+}
+
