e37f427ebcf7c3e8674d66072d3b269e832a8865
AUTO SERVER <vt001@ASs-Mac-mini.local>
2019-11-26 17:02:53 +0700

    done demo 4

---
+++ ImpleControllersAndViews/Demo4/Demo4/VC1ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  VC1ViewController.swift
+//  Demo4
+//
+//  Created by AUTO SERVER on 11/26/19.
+//  Copyright © 2019 AUTO SERVER. All rights reserved.
+//
+
+import UIKit
+
+class VC1ViewController: UIViewController {
+
+    @IBOutlet weak var lb: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        lb.text = "ajkakdjhaskfhakljfahdkljfahldjhajkdfhaljdfhakldjhakljhdlakjdhklajdhflkjahfkljashkljhklajsdhfklsadjh"
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ImpleControllersAndViews/Demo4/Demo4/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Demo4
+//
+//  Created by AUTO SERVER on 11/26/19.
+//  Copyright © 2019 AUTO SERVER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lb: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        lb.text = "agajshdgajhdgjashdgajhgadhgajdhgajkhdfgjkahfgjshdgfjhasdgfjhsgfjhsgdfjsdhfgajksdhfgajkshfgasjkhhdgjashdgajhgadhgajdhgajkhdfgjkahfgjshdgfjhasdgfjhsgfjhsgdfjsdhfgajksdhfgajkshfgasjkhhdgjashdgajhgadhgajdhgajkhdfgjkahfgjshdgfjhasdgfjhsgfjhsgdfjsdhfgajksdhfgajkshfgasjkh"
+    }
+
+
+}
+
