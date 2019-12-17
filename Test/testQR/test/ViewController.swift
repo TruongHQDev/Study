1bc6d540db815afd5a1d951cdd58ada34d1172c6
AUTO SERVER <vt001@ASs-Mac-mini.local>
2019-12-17 14:39:53 +0700

    update test qr

---
+++ Test/testQR/test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by AUTO SERVER on 12/17/19.
+//  Copyright © 2019 AUTO SERVER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
